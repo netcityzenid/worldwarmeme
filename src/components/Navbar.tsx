8d627c60c36f88e4781471a93a611f101cad4e4a
Netcityzen <netcityzen.id@gmail.com>
2024-08-10 12:47:38 +0700

    update fe

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 export default function Navbar() {
-    return(
-        <div className=" px-5 h-16 items-center bg-black/50 w-full flex relative z-[100] text-white font-light text-2xl">World War Meme</div>
-    )
+  return (
+    <div className="relative z-[100] flex justify-center items-center w-full px-5">
+      <div className="flex justify-center w-full items-center">
+        <img src="/images/logo.png" alt="pepe" className="mt-0 h-8 w-auto mr-1 "></img>
+        <div className="  h-16 items-center  flex z-[100] text-xl uppercase font-semibold text-yellow-500">World War Meme</div>
+      </div>
+      
+    </div>
+  );
 }
